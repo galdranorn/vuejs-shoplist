2e75eb8e740761d889721c3923b0e44f78ee740f
Justyna Skiberowska <skiberowska.justyna@wp.pl>
2019-07-10 19:00:53 +0200

    Initial commit

---
+++ src/components/Shoplist.vue
@@ -0,0 +1,18 @@
+<template>
+  <div class="shoplist">
+    Shoplist
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'Shoplist',
+  props: {
+  }
+}
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+
+</style>
