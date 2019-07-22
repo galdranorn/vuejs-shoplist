9218024563d08eaa77192867a2c12d43a7eb3a48
Justyna Skiberowska <skiberowska.justyna@wp.pl>
2019-07-22 16:30:46 +0200

    Learn about forms and validation (vee-validate)

--- src/components/Shoplist.vue
+++ src/components/Shoplist.vue
@@ -1,12 +1,17 @@
 <template>
   <div class="shoplist">
     <div class="holder">
+
+      <form @submit.prevent="addProduct">
+        <input type="text" placeholder="Enter what you want to buy..." v-model="product" v-validate="'min:3'" name="product"/>
+        <p class="alert" v-if="errors.has('product')">{{ errors.first('product') }}</p>
+      </form>
+      
       <p class="header" v-if="shoplist.length>0">There is your shoplist:</p>
       <p class="header" v-else>There is no products on the shoplist</p>
       <ul class="shoplist">
-        <li v-for="(data, index) in shoplist" :key='index'> {{ index }}. {{ data.product }}</li>
+        <li v-for="(data, index) in shoplist" :key='index'>{{ data.product }}</li>
       </ul>
-      <div v-bind:style="{ backgroundColor: bgClr, widht: bgWth, height: bgHth }"></div>
     </div>
   </div>
 </template>
@@ -16,13 +21,25 @@ export default {
   name: 'Shoplist',
   data() {
     return {
+      product: '',
       shoplist: [
         { "product": "cheese" },
         { "product": "beef steak" },
       ],
-      bgClr: 'green',
-      bgWth: '100%',
-      bgHth: '30px',
+    }
+  },
+  methods: {
+    addProduct() {
+      this.$validator.validateAll().then((result) => {
+        if (result) {
+          this.shoplist.push({product: this.product});
+          this.product = '';
+        }
+        else {
+          //console.log('Not valid')
+        }
+      })
+      
     }
   }
 }
@@ -30,14 +47,49 @@ export default {
 
 <!-- Add "scoped" attribute to limit CSS to this component only -->
 <style scoped>
+  .holder {
+    background: #fff;
+  }
+
+  ul {
+    margin: 0;
+    padding: 0;
+    list-style-type: none;
+  }
+  
+  ul li {
+    padding: 20px;
+    font-size: 1.3em;
+    background-color: #E0EDF4;
+    border-left: 5px solid #3EB3F6;
+    margin-bottom: 2px;
+    color: #3E5252;
+  }
+
+  p {
+    text-align:center;
+    padding: 30px 0;
+    color: gray;
+  }
+
+  .container {
+    box-shadow: 0px 0px 40px lightgray;
+  }
+
+  input {
+    width: calc(100% - 40px);
+    border: 0;
+    padding: 20px;
+    font-size: 1.3em;
+    background-color: #323333;
+    color: #687F7F;
+  }
+
   .alert {
-    background-color: rgb(179, 179, 121);
-    width: 100%;
-    height: 30px;
+    background: #fdf2ce;
+    display: inline-block;
+    padding: 5px;
+    margin-top: -20px;
   }
-  .hello {
-    background-color: red;
-    width: 100%;
-    height: 30px;
-  }
+
 </style>
